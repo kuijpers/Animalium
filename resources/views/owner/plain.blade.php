5c5ba2087c3cf5461c9327d94d29ce480ea5e9b5
kuijpers <darkknightworks@gmail.com>
2017-05-02 16:47:06 +0200

    Added plain template files for Admin and Owner to use for new pages

---
+++ resources/views/owner/plain.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.owner.main')
+
+@section('content')
+
+        <div class="row">
+                <div class="col-lg-12">
+                        Plain page. Fill it up with your stuff
+                </div>
+
+        </div>
+
+@endsection
