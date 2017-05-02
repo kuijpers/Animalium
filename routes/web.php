75f0813d58d7d37de9fa64f34824233e75a4cb1f
kuijpers <darkknightworks@gmail.com>
2017-05-02 11:21:12 +0200

    Added Owner routes

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,17 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 
-	Route::prefix('admin')->group(function(){
+Route::prefix('admin')->group(function(){
 
-		Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-		Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-		Route::get('/', 'AdminController@index')->name('admin.dashboard');
-	});
+	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
+
+
+Route::prefix('owner')->group(function(){
+
+	Route::get('/login', 'Auth\OwnerLoginController@showLoginForm')->name('owner.login');
+	Route::post('/login', 'Auth\OwnerLoginController@login')->name('owner.login.submit');
+	Route::get('/', 'OwnerController@index')->name('owner.dashboard');
+});
